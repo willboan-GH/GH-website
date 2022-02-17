ae3ca01b98b7f16f77637d8742628117882eac96
willboan-GH <99904091+willboan-GH@users.noreply.github.com>
2022-02-17 15:09:34 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 $(function(){
 
-  console.log($("#footer-1"));
+
+  // $("#title").hide();
+  // $("#text-animated").hide();
+  $("#title,#teal-rectangle").fadeIn(1000).css('display', 'inline-block');
+  setTimeout(function() {
+    $("#text-animated").fadeIn(1200).css('display', 'inline-block');
+  },600);
+
+
+
+
+
+
+
+
+  // function typeWriter(id, text, speed) {
+  //   var i = 0;
+  //   if (i < text.length) {
+  //     document.getElementById(id).innerHTML += text.charAt(i);
+  //     i++;
+  //     setTimeout(typeWriter, speed);
+  //   }
+  // }
+  //
+  // typeWriter(
+  //   "text-animated",
+  //   'We develop advanced talent strategies for global businesses across all industries, from start-ups to leading multinationals.',
+  //   50
+  // );
 
 });
