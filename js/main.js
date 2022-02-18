89999842fed38f656313abda76e28d826a1bdcba
willboan-GH <99904091+willboan-GH@users.noreply.github.com>
2022-02-17 20:51:47 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,58 @@
 $(function(){
 
 
-  // $("#title").hide();
-  // $("#text-animated").hide();
-  $("#relaunching").fadeIn(800).css('display', 'inline-block');
-  setTimeout(function() {
-    $("#title").fadeIn(1000).css('display', 'inline-block');
-  },1800);
-  setTimeout(function() {
-    $("#text-animated,#teal-rectangle").fadeIn(1200).css('display', 'inline-block');
-  },2400);
+  // $("#relaunching").fadeIn(800, function() {
+  //   $("#logo-cont-TalentFlo").fadeIn(600).css('display', 'inline-block').fadeOut(400, function() {
+  //     $("#logo-cont-TalentConnect").fadeIn(600).css('display', 'inline-block').fadeOut(400, function() {
+  //       $("#logo-cont-TalentSelect").fadeIn(600).css('display', 'inline-block').fadeOut(400, function() {
+  //         $("#logo-cont-TalentSUV").fadeIn(600).css('display', 'inline-block').fadeOut(400, function() {
+  //           $("#title-cont").fadeIn(1000).css('display', 'inline-block');
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
+
+  $("#relaunching")
+  .fadeIn(1300).fadeOut(500)
+  .fadeIn(1300);
+
+  $("#title-cont-1").fadeIn(1800, function() {
+    $("#logo-cont-TalentFlo").fadeIn(1800, function() {
+      $("#logo-cont-TalentConnect").fadeIn(1800, function() {
+      //   $("#logo-cont-TalentSelect").fadeIn(1800, function() {
+          $("#logo-cont-TalentSUV").fadeIn(1800, function() {
+            $("#title-cont-2").fadeIn(1800).css('display', 'inline-block');
+            setTimeout(function() {
+              $("#text-animated,#teal-rectangle").fadeIn(800).css('display', 'inline-block');
+            }, 1200);
+          }).css('display', 'inline-block').fadeOut(500);
+      //   }).css('display', 'inline-block').fadeOut(500);
+      }).css('display', 'inline-block').fadeOut(500);
+    }).css('display', 'inline-block').fadeOut(500);
+  }).css('display', 'inline-block').fadeOut(500);
+
+
+  // $("#relaunching").fadeIn(800, function() {
+  //   $("#title-cont-1").fadeIn(1800, function() {
+  //     $("#relaunching").fadeOut(500);
+  //     $("#logo-cont-TalentFlo").fadeIn(1800, function() {
+  //       $("#logo-cont-TalentConnect").fadeIn(1800, function() {
+  //         $("#logo-cont-TalentSelect").fadeIn(1800, function() {
+  //           $("#logo-cont-TalentSUV").fadeIn(1800, function() {
+  //             $("#title-cont-2").fadeIn(1800).css('display', 'inline-block');
+  //             setTimeout(function() {
+  //               $("#text-animated,#teal-rectangle").fadeIn(800).css('display', 'inline-block');
+  //             }, 1200);
+  //           }).css('display', 'inline-block').fadeOut(500);
+  //         }).css('display', 'inline-block').fadeOut(500);
+  //       }).css('display', 'inline-block').fadeOut(500);
+  //     }).css('display', 'inline-block').fadeOut(500);
+  //   }).css('display', 'inline-block').fadeOut(500);
+  // });
+
+
+
 
 
 
